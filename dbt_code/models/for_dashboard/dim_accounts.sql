92932b6ad08c0cb1c7bef588d6f4679507f0d27f
Patrick F Callahan <pfc1047@proton.me>
2026-07-03 17:58:44 -0400

    claude's re-auth approach

--- dbt_code/models/for_dashboard/dim_accounts.sql
+++ dbt_code/models/for_dashboard/dim_accounts.sql
@@ -7,8 +7,8 @@ from read_json_auto({{ source('nfcu_raw', 'transactions')}})
 select acnt.id as bank_account_id
       ,acnt.name as bank_account_name
       ,acnt.currency
-      ,acnt.balance as actual_balance
-      ,acnt."available-balance" as available_balance
+      ,try_cast(acnt.balance as decimal(14,2)) as actual_balance
+      ,try_cast(acnt."available-balance" as decimal(14,2)) as available_balance
       ,to_timestamp(acnt."balance-date") as balance_as_of_timestamp
       ,filename
       ,get_current_timestamp() as record_loaded_at_timestamp
