638b9e3475d70d5766f06ee70a36cecec3bdc029
Patrick F Callahan <pfc1047@proton.me>
2026-07-03 16:29:56 -0400

    accounts table-- for top level balances

---
+++ dbt_code/models/for_dashboard/dim_accounts.sql
@@ -0,0 +1,20 @@
+{{ config(
+    external_location='s3://pfc-nfcu/datamart/dim_account.parquet'
+) }}
+
+with init as (
+select unnest(accounts) as acnt, filename
+from read_json_auto('s3://pfc-nfcu/transactions/*/*/*/*.json')
+)
+
+select acnt.id as bank_account_id
+,acnt.name as bank_account_name
+,acnt.currency
+,acnt.balance as actual_balance
+,acnt."available-balance" as available_balance
+,to_timestamp(acnt."balance-date") as balance_as_of_date, filename
+from init
+qualify row_number() over (
+partition by bank_account_id
+order     by balance_as_of_date desc
+) = 1
