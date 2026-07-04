b900db38f4801734e74e303f06ae541be83df8c4
Patrick F Callahan <pfc1047@proton.me>
2026-07-03 21:02:15 -0400

    Adding transactions table

---
+++ dbt_code/models/for_dashboard/fact_transactions.sql
@@ -0,0 +1,21 @@
+WITH raw AS (
+  SELECT UNNEST(accounts) AS acnt
+      ,filename
+from read_json_auto({{ source('nfcu_raw', 'transactions')}})
+)
+SELECT
+  acnt.id AS bank_account_id,
+  acnt.name AS bank_account_name,
+  t.id AS txn_id,
+  t.description,
+  t.payee,
+  CAST(t.amount AS DECIMAL(14,2))       AS amount,
+  to_timestamp(t.posted)                AS posted_at_timestamp,
+  filename,
+  get_current_timestamp() as record_loaded_at_timestamp
+FROM raw, UNNEST(acnt.transactions) AS _(t)
+qualify row_number() over (
+    partition by t.id 
+    order     by to_timestamp(t.posted) desc
+) = 1
+
