70965364b58bf3ef572423b27649b3e62da20c36
Patrick F Callahan <pfc1047@proton.me>
2026-07-04 11:43:17 -0400

    name change, general model overhaul

--- dbt_code/models/stage/map_merchant_category.sql
+++ dbt_code/models/stage/map_merchant_category.sql
@@ -1,4 +1,9 @@
-{{ config(materialized='external', format='parquet', location='s3://pfc-nfcu/stage/stg_merchant_category.parquet') }}
+{{ config(materialized='external', format='parquet', location='s3://pfc-nfcu/stage/map_merchant_category.parquet') }}
 -- passthrough model required when reading from seeds
 -- seeds to not support external table strategy; need a passthrough node
-select * from {{ ref('seed_merchant_category') }}
+select md5(merchant_name) as merchant_key
+      ,merchant_name
+      ,merchant_category
+      ,merchant_subcategory
+      ,try_cast(last_updated_timestamp as timestamptz) as last_updated_timestamp
+from {{ ref('seed_merchant_category') }}
