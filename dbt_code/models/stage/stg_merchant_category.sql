406314dad59d7d5af284f2efd3c4a8f13b720cad
Patrick F Callahan <pfc1047@proton.me>
2026-07-04 12:14:56 -0400

    making sure merchant_key is relating correctly

--- dbt_code/models/stage/stg_merchant_category.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized='external', format='parquet', location='s3://pfc-nfcu/stage/stg_merchant_category.parquet') }}
--- passthrough model required when reading from seeds
--- seeds to not support external table strategy; need a passthrough node
-select * from {{ ref('seed_merchant_category') }}
