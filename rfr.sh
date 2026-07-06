e31bd391d8065b72521d4e9848a937f0d77c9fd8
Patrick F Callahan <pfc1047@proton.me>
2026-07-06 16:28:24 -0400

    making the whole thing fail if one bit fails

--- rfr.sh
+++ rfr.sh
@@ -1,5 +1,4 @@
 # !/bin/bash/
-cd $HOMEDIR/pfc-bank/dbt_code
-uv run dbt build 
-cd .. && cd dashboard
-npm run sources && npm run dev
+# chaining like this makes the whole thing fail if one bit fails
+# which i think is what i want, so keeping like this for now
+cd $HOMEDIR/pfc-bank/dbt_code && uv run dbt build && cd .. && cd dashboard &&npm run sources && npm run dev
