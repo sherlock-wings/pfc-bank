3f3a83db1f39404d71e459013587d76c111808e1
Patrick F Callahan <pfc1047@proton.me>
2026-07-12 13:12:31 -0400

    I just think this is prettier

--- dbt_code/models/dashboard_mart/kimball_star/fact_daily_pay.sql
+++ dbt_code/models/dashboard_mart/kimball_star/fact_daily_pay.sql
@@ -2,7 +2,8 @@
 
 select a.full_date as calendar_date
       ,a.date_quarter as calendar_quarter
-      ,try_cast((b.biweekly_paycheck_posttax_amount*2)/a.total_days_in_month as decimal(12,2)) as daily_income
+      ,try_cast((b.biweekly_paycheck_posttax_amount*2)/a.total_days_in_month as decimal(12,2)) 
+       as daily_income
       ,b.employer_name as from_employer
 from {{ ref('dim_date') }} a 
 left join {{ ref('stg_payrate') }} b
