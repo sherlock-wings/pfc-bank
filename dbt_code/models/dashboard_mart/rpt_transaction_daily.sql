9ddc3987d2dd82bee211cdc0233ace287c09c1c2
Patrick F Callahan <pfc1047@proton.me>
2026-07-16 12:54:58 -0400

    finalizing this table

--- dbt_code/models/dashboard_mart/rpt_transaction_daily.sql
+++ dbt_code/models/dashboard_mart/rpt_transaction_daily.sql
@@ -2,16 +2,17 @@
 
 with money_spine as (
     select calendar_date
-           calendar_date - interval 1 month as last_month_date
-          ,sum(txn_amount) as eod_balance
+          ,calendar_date - interval 1 month as last_month_date
+          ,count(case when txn_amount is not null then 1 end) as eod_txn_count 
+          ,coalesce(sum(txn_amount),0) as eod_balance
     from {{ ref('rpt_transaction_detail')}}
     group by 1 
 )
 
 select a.calendar_date
-      ,a.eod_balance
-      ,b.eod_balance as last_month_eod_balance
-      ,b.eod_balance - a.eod_balance as mom_dollars_change
+      ,try_cast(a.eod_balance as decimal(12,2)) as eod_balance
+      ,try_cast(b.eod_balance as decimal(12,2)) as last_month_eod_balance
+      ,try_cast(b.eod_balance - a.eod_balance as decimal(12,2)) as mom_dollars_change
       ,case 
          when b.eod_balance <> 0
          then try_cast(round((a.eod_balance - b.eod_balance)/abs(b.eod_balance),4) as decimal (14,4))
